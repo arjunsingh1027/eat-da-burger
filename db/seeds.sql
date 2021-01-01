1bd1ba3c6cd5e177360565278624d9e8b59d5a7b
arjunsingh <arjunksingh1027@gmail.com>
2021-01-01 13:37:41 -0700

    init commit

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger) VALUES ('Single');
+INSERT INTO burgers (burger) VALUES ('Double');
+INSERT INTO burgers (burger) VALUES ('Diablo');
+INSERT INTO burgers (burger) VALUES ('Cowboy');
+INSERT INTO burgers (burger) VALUES ('Diner');
