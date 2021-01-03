5957874cbd69a329ac3922944283cd17b7312088
arjunsingh <arjunksingh1027@gmail.com>
2021-01-03 12:57:39 -0700

    orm and config

--- config/orm.js
+++ config/orm.js
@@ -1,6 +1,53 @@
 // Import MySQL connection.
-var connection = require("./connection.js");
+const connection = require("./connection.js");
+const util = require("util");
 
+const orm = {
+    selectAll: function (table, cb) {
+        return connection.query('SELECT * FROM ??', [table], function (err, res) {
+            if (err) {
+                throw err;
+            }
+            cb(data);
+        });
+    },
+
+    insertBurger: function (table, columnName, burger_name, cb) {
+        const query = 'INSERT INTO ?? (??) VALUES (?)';
+
+        connection.query(
+            query,
+            [table, columnName, burger_name],
+            function (err, data) {
+                if (err){
+                    throw err;
+                }
+                cb(data);
+            }
+        );
+    },
+
+    updateBurger: function (table, condition, id, cb) {
+        const query = 'UPDATE ?? SET eaten = ? WHERE id = ?';
+
+        connection.query(
+            query,
+            [table, condition, id],
+            function (err, data) {
+                if (err){
+                    throw err;
+                }
+                cb(data);
+            }
+        );
+    },
+
+    deleteBurger: function (table, id, cb){
+        const query = 'DELETE FROM ?? WHERE id = ?';
+
+        connection.query(query, [table, id], cb);
+    }
+};
 
 // Export the orm object for the model (cat.js).
 module.exports = orm;
--- db/schema.sql
+++ db/schema.sql
@@ -5,6 +5,6 @@ USE burgers_db;
 CREATE TABLE burgers
 (
 	id int NOT NULL AUTO_INCREMENT,
-	burger varchar(255) NOT NULL,
+	burger_name varchar(255) NOT NULL,
 	eaten BOOLEAN DEFAULT false,
 );
